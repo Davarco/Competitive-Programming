c8399f12868ecabc6626a72cf212daeb799e7784
David Zhang <davidwzhang@berkeley.edu>
2020-06-01 22:12:36 -0700

    hmm

--- Lib/segment_tree.cpp
+++ Lib/segment_tree.cpp
@@ -1,64 +1,55 @@
-// Modified from Competitive Programming 3.
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-// Perform Range Min/Max/Sum Queries, the index of the smallest number between 
-// two indices in a list of numbers.
-//
-// 0-indexed, inclusive, returns indexes.
 class SegmentTree 
 {
 private:
-	vector<int> st, A;
-	int N;
-
-	int left(int p) 
-	{
-		return p << 1;
-	}
-
-	int right(int p)
-	{
-		return (p << 1) + 1;
-	}
+	vector<int> st, A; int N;
+	int LT(int i) { return (i << 1); }
+	int RT(int i) { return (i << 1) + 1; }
 
 	// O(N)
-	void build(int p, int L, int R)
+	void build(int i, int L, int R)
 	{
 		if (L == R)
-		{
-			// st[p] = L; // Range Min/Max Queries
-			st[p] = A[L]; // Range Sum Queries
-		}
+			st[i] = A[L];
 		else
 		{
-			build(left(p), L, (L+R)/2);
-			build(right(p), (L+R)/2+1, R);
-			int p1 = st[left(p)], p2 = st[right(p)];
-			// st[p] = A[p1] < A[p2] ? p1 : p2; // Range Min Queries
-			// st[p] = A[p1] > A[p2] ? p1 : p2; // Range Max Queries
-			st[p] = p1 + p2; // Range Sum Queries
+			build(LT(i), L, (L+R)/2);
+			build(RT(i), (L+R)/2+1, R);
+			int a = st[LT(i)], b = st[RT(i)];
+			st[i] = a + b;
 		}
 	}
 
 	// O(log N)
-	int rmq(int p, int L, int R, int i, int j)
+	int rq(int i, int L, int R, int a, int b)
 	{
-		if (i > R || j < L)
-			return -1;
-		if (L >= i && R <= j) 
-			return st[p];
-		int p1 = rmq(left(p), L, (L+R)/2, i, j);
-		int p2 = rmq(right(p), (L+R)/2+1, R, i, j);
-		if (p1 == -1) 
-			return p2;
-		if (p2 == -1)
-			return p1;
-		// return A[p1] < A[p2] ? p1 : p2; // Range Min Queries
-		// return A[p1] > A[p2] ? p1 : p2; // Range Max Queries
-		return p1 + p2; // Range Sum Queries
+		if (a > R || b < L) return -1;
+		if (L >= a && R <= b) return st[i];
+		int r1 = rq(LT(i), L, (L+R)/2, a, b);
+		int r2 = rq(RT(i), (L+R)/2+1, R, a, b);
+		if (r1 == -1) return r2;
+		if (r2 == -1) return r1;
+		return r1 + r2;
+	}
+
+	void update(int i, int L, int R, int a, int v)
+	{
+		if (L == R)
+		{
+			A[a] = v;
+			st[i] = v;
+		}
+		else
+		{
+			if (a >= L && a <= (L+R)/2) update(LT(i), L, (L+R)/2, a, v);
+			else update(RT(i), (L+R)/2+1, R, a, v);
+			int a = st[LT(i)], b = st[RT(i)];
+			st[i] = a + b;
+		}
 	}
 
 public:
@@ -70,18 +61,21 @@ public:
 		build(1, 0, N-1);
 	}
 
-	int rmq(int i, int j)
-	{
-		return rmq(1, 0, N-1, i, j);
-	}
+	int rq(int a, int b) { return rq(1, 0, N-1, a, b); }
+	
+	int update(int a, int v) { update(1, 0, N-1, a, v); }
 };
 
 int main()
 {
 	// SegmentTree st({18, 17, 13, 19, 15, 11, 20});
-	SegmentTree st({2, 2, 4, 4, 4, 4, 1, 3, 3, 3});
-	cout << st.rmq(3, 4) << endl;
-	cout << st.rmq(6, 9) << endl;
+	SegmentTree st({2, 2, 4, 4, 4, 4, 1, 3, 3});
+	cout << st.rq(3, 4) << endl;
+	st.update(3, 90);
+	cout << st.rq(6, 8) << endl;
+	cout << st.rq(0, 4) << endl;
+	cout << st.rq(2, 2) << endl;
 	// cout << st.rmq(1, 3) << endl;
 	// cout << st.rmq(4, 6) << endl;
 }
+
